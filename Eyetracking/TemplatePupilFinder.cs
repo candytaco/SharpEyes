1e5d1b8119b3d952f8082cca88db6c2c9105e93d
tzhang <t.zhang@berkeley.edu>
2021-02-01 15:18:50 -0700

    Skeleton on custom templates and also bugfix not storing best correlation

--- Eyetracking/TemplatePupilFinder.cs
+++ Eyetracking/TemplatePupilFinder.cs
@@ -18,9 +18,23 @@ namespace Eyetracking
 		private Mat[] matchResults;
 		private double bestCorrelationOnThisFrame;
 
+		/// <summary>
+		/// Because there is an option to use a part of a frame as a template,
+		/// there is no good way to determine pupil size if that is the case. So we store
+		/// the value from that frame here and apply it onwards.
+		/// The nullable-ness also doubles as an indicator of whether we are using
+		/// the frame as a template. if null, we are using generated templates.
+		/// </summary>
+		private Nullable<double> storedPupilSize = null;
+
 		public TemplatePupilFinder(string videoFileName, System.Windows.Controls.ProgressBar progressBar,
 								   SetStatus setStatus, FrameProcessed updateFrame)
 			: base(videoFileName, progressBar, setStatus, updateFrame)
+		{
+			MakeTemplates();
+		}
+
+		public void MakeTemplates()
 		{
 			templates = new Mat[maxRadius - minRadius + 1];
 			matchResults = new Mat[maxRadius - minRadius + 1];
@@ -30,6 +44,14 @@ namespace Eyetracking
 				matchResults[i] = new Mat();
 				Cv2.Circle(templates[i], maxRadius + 1, maxRadius + 1, i + minRadius, 0, -1);   // negative thickness == filled
 			}
+			storedPupilSize = null;
+		}
+
+		public void UseImageSegmentAsTemplate(int top, int bottom, int left, int right, double radius)
+		{
+			templates[0] = new Mat(bottom - top + 1, right - left + 1, MatType.CV_8UC1);
+			grayFrame[top, bottom, left, right].CopyTo(templates[0]);
+			storedPupilSize = radius;
 		}
 
 		public override void FindPupils(int Frames)
@@ -42,6 +64,7 @@ namespace Eyetracking
 			};
 			worker.DoWork += delegate (object sender, DoWorkEventArgs args)
 			{
+				// TODO: perhaps parallelize over frames rather than templates
 				object templateLock = new object();
 				for (int f = 0; f < Frames; f++)
 				{
@@ -59,6 +82,7 @@ namespace Eyetracking
 								pupilLocations[CurrentFrameNumber, 0] = maxLocation.X + maxRadius + left;
 								pupilLocations[CurrentFrameNumber, 1] = maxLocation.Y + maxRadius + top;
 								pupilLocations[CurrentFrameNumber, 2] = i + minRadius;
+								bestCorrelationOnThisFrame = maxVal;
 							}
 						}
 					});
