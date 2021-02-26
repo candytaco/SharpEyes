73319a3638801861e8d3b148a0a6d08a5dfbd9a2
candytaco <t.zhang@berkeley.edu>
2021-02-26 14:26:06 -0700

    Skeleton for viewing gaze location on Stimulus

---
+++ Eyetracking/StimulusGazeViewer.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Eyetracking
+{
+    /// <summary>
+    /// Interaction logic for StimulusGazeViewer.xaml
+    /// </summary>
+    public partial class StimulusGazeViewer : Window
+    {
+        public StimulusGazeViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenVideoMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LoadGazeMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveGazeMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MoveGazeButtom_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Canvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void VideoMediaElement_MediaEnded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void VideoSlider_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void VideoSlider_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void VideoSlider_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        {
+
+        }
+
+        private void VideoSlider_DragStarted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
+        {
+
+        }
+
+        private void SetCurrentAsEyetrackingStartButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AutoFindEyetrackingStartButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PlayPauseButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PreviousKeyFrameButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NextKeyFrameButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveGazeAsMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
