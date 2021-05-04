487077642a317ce0e7ac39baed30fcaff5908a12
candytaco <t.zhang@berkeley.edu>
2021-05-04 11:58:13 -0700

    Avalonia skeleton

---
+++ SharpEyes/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpEyes.ViewModels
+{
+	public class ViewModelBase : ReactiveObject
+	{
+	}
+}
