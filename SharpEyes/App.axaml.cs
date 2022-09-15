13227b8aa0df234e5d28f3bdf5708091600a8c25
tzhang <t.zhang@berkeley.edu>
2022-09-15 09:52:24 -0700

    More avalonia

--- SharpEyes/App.axaml.cs
+++ SharpEyes/App.axaml.cs
@@ -3,17 +3,15 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using SharpEyes.ViewModels;
 using SharpEyes.Views;
-
 using Sentry;
 
 namespace SharpEyes
 {
-	public class App : Application
+	public partial class App : Application
 	{
 		public override void Initialize()
 		{
 			AvaloniaXamlLoader.Load(this);
-			SentrySdk.Init("https://4aa216608a894bd99da3daa7424c995d@o553633.ingest.sentry.io/5689896");
 		}
 
 		public override void OnFrameworkInitializationCompleted()
@@ -27,6 +25,8 @@ namespace SharpEyes
 			}
 
 			base.OnFrameworkInitializationCompleted();
+
+			SentrySdk.Init("https://4aa216608a894bd99da3daa7424c995d@o553633.ingest.sentry.io/5689896");
 		}
 	}
 }
--- SharpEyes/Views/MainWindow.axaml.cs
+++ SharpEyes/Views/MainWindow.axaml.cs
@@ -1,86 +1,12 @@
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Interactivity;
-using Avalonia.Markup.Xaml;
 
 namespace SharpEyes.Views
 {
-	public class MainWindow : Window
+	public partial class MainWindow : Window
 	{
 		public MainWindow()
 		{
 			InitializeComponent();
-#if DEBUG
-            this.AttachDevTools();
-#endif
-			ExtendClientAreaTitleBarHeightHint = -1;
-
-			TransparencyLevelHint = WindowTransparencyLevel.AcrylicBlur;
-		}
-
-		private void InitializeComponent()
-		{
-			AvaloniaXamlLoader.Load(this);
-		}
-
-		private void OpenFileMenuItem_OnClick(object? sender, RoutedEventArgs e)
-		{
-			throw new System.NotImplementedException();
-		}
-
-		private void LoadSavedDataMenuItem_OnClick(object? sender, RoutedEventArgs e)
-		{
-			throw new System.NotImplementedException();
-		}
-
-		private void LoadSavedEyetrackingMenuItem_OnClick(object? sender, RoutedEventArgs e)
-		{
-			throw new System.NotImplementedException();
-		}
-
-		private void LoadSavedTemplatesMenuItem_OnClick(object? sender, RoutedEventArgs e)
-		{
-			throw new System.NotImplementedException();
-		}
-
-		private void SaveTimestampsMenuItem_OnClick(object? sender, RoutedEventArgs e)
-		{
-			throw new System.NotImplementedException();
-		}
-
-		private void SaveEyetrackingMenuItem_OnClick(object? sender, RoutedEventArgs e)
-		{
-			throw new System.NotImplementedException();
-		}
-
-		private void SaveTemplatesMenuItem_OnClick(object? sender, RoutedEventArgs e)
-		{
-			throw new System.NotImplementedException();
-		}
-
-		private void SaveAllMenuItem_OnClick(object? sender, RoutedEventArgs e)
-		{
-			throw new System.NotImplementedException();
-		}
-
-		private void StimulusViewMenuItem_OnClick(object? sender, RoutedEventArgs e)
-		{
-			throw new System.NotImplementedException();
-		}
-
-		private void HelpMenuItem_OnClick(object? sender, RoutedEventArgs e)
-		{
-			throw new System.NotImplementedException();
-		}
-
-		private void ReportBugmenuItem_OnClick(object? sender, RoutedEventArgs e)
-		{
-			throw new System.NotImplementedException();
-		}
-
-		private void AboutMenuItem_OnClick(object? sender, RoutedEventArgs e)
-		{
-			throw new System.NotImplementedException();
 		}
 	}
 }
