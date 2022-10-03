02153c2dc1a25e88318ba11ea8ce9f995ac134d4
tzhang <t.zhang@berkeley.edu>
2022-10-03 12:13:55 -0700

    convert template pupil finder options usercontrol interactions to command bindings

--- SharpEyes/ViewModels/TemplatePupilFinderConfigUserControlViewModel.cs
+++ SharpEyes/ViewModels/TemplatePupilFinderConfigUserControlViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using Avalonia.Media.Imaging;
+using Eyetracking;
+using OpenCvSharp.Flann;
 using ReactiveUI;
 using System;
 using System.Collections.Generic;
@@ -15,11 +17,11 @@ namespace SharpEyes.ViewModels
 
 		// Commands
 		public ReactiveCommand<Unit, Unit>? AddCurrentAsTemplateCommand { get; } = null;
-		public ReactiveCommand<Unit, Unit>? RemoveCurrentTemplateCommand { get; } = null;
+		public ReactiveCommand<Unit, Unit>? RemoveCurrentTemplateCommand { get; private set; } = null;
 		public ReactiveCommand<int, Unit>? ChangeTemplatePreviewIndexCommand { get; } = null;
-		public ReactiveCommand<Unit, Unit>? ResetTemplatesCommand { get; } = null;
+		public ReactiveCommand<Unit, Unit>? ResetTemplatesCommand { get; private set; } = null;
 		public ReactiveCommand<Unit, Unit>? AddCurrentAsAntiTemplateCommand { get; } = null;
-		public ReactiveCommand<Unit, Unit>? RemoveCurrentAntiTemplateCommand { get; } = null;
+		public ReactiveCommand<Unit, Unit>? RemoveCurrentAntiTemplateCommand { get; private set; } = null;
 		public ReactiveCommand<int, Unit>? ChangeAntiTemplatePreviewIndexCommand { get; } = null;
 
 		// templates section
@@ -68,6 +70,21 @@ namespace SharpEyes.ViewModels
 		public int FractionOfTemplatesToUse { get; set; } = 75;
 		public int NumTemplatesToMatch { get; set; } = 2;
 
+
+		// ============
+		// Logic stuffs
+		// ============
+
+		public TemplatePupilFinder? TemplatePupilFinder
+		{
+			get
+			{
+				if (Parent.pupilFinder is TemplatePupilFinder templateFinder)
+					return templateFinder;
+				return null;
+			}
+		}
+
 		// view model hierarchy
 		public PupilFindingUserControlViewModel? Parent { get; private set; } = null;
 		
@@ -92,12 +109,37 @@ namespace SharpEyes.ViewModels
 
 		public void AddCurrentAsTemplate()
 		{
+			if (TemplatePupilFinder != null)
+			{
+				int top = (int)(Parent.PupilY - Parent.PupilRadius * 1.5);
+				int bottom = (int)(Parent.PupilY + Parent.PupilRadius * 1.5 + 2);
+				int left = (int)(Parent.PupilX - Parent.PupilRadius * 1.5);
+				int right = (int)(Parent.PupilX + Parent.PupilRadius * 1.5 + 2);
+				TemplatePupilFinder.AddImageSegmentAsTemplate(top, bottom, left, right, Parent.PupilRadius);
+				SetTemplatePreviewIndex(TemplatePupilFinder.NumTemplates);
 
+				/* TODO: enabling buttons
+				if (!saveTemplatesMenuItem.IsEnabled)
+				{
+					saveTemplatesMenuItem.IsEnabled = true;
+				*/
+				ResetTemplatesCommand ??= ReactiveCommand.Create(ResetTemplates);
+
+
+				if (TemplatePupilFinder.NumTemplates > 1 && RemoveCurrentTemplateCommand == null)
+					RemoveCurrentTemplateCommand = ReactiveCommand.Create(RemoveCurrentTemplate);
+			}
 		}
 
 		public void RemoveCurrentTemplate()
 		{
-
+			if (TemplatePupilFinder != null)
+			{
+				TemplatePupilFinder.RemoveTemplate(CurrentTemplateIndex);
+				SetTemplatePreviewIndex(CurrentTemplateIndex - 1);
+				if (TemplatePupilFinder.NumTemplates < 2)
+					RemoveCurrentTemplateCommand = null;
+			}
 		}
 
 		/// <summary>
@@ -106,27 +148,83 @@ namespace SharpEyes.ViewModels
 		/// <param name="delta">-1 to show previous, +1 to show next</param>
 		public void ChangeTemplatePreviewIndex(int delta)
 		{
-			return;
+			SetTemplatePreviewIndex(CurrentTemplateIndex + delta);
 		}
 
 		public void ResetTemplates()
 		{
-
+			if (TemplatePupilFinder != null)
+			{
+				TemplatePupilFinder.MakeTemplates();
+				SetTemplatePreviewIndex(0);
+				ResetTemplatesCommand = null;
+				RemoveCurrentTemplateCommand = null;
+				RemoveCurrentAntiTemplateCommand = null;
+			}
 		}
 
 		public void AddCurrentAsAntiTemplate()
 		{
+			if (TemplatePupilFinder != null)
+			{
+				int top = (int)(Parent.PupilY - Parent.PupilRadius * 1.5);
+				int bottom = (int)(Parent.PupilY + Parent.PupilRadius * 1.5 + 2);
+				int left = (int)(Parent.PupilX - Parent.PupilRadius * 1.5);
+				int right = (int)(Parent.PupilX + Parent.PupilRadius * 1.5 + 2);
+				TemplatePupilFinder.AddImageSegmentAsAntiTemplate(top, bottom, left, right);
+				SetAntiTemplatePreviewIndex(TemplatePupilFinder.NumAntiTemplates);
 
+				/* TODO: enabling buttons
+				if (!saveTemplatesMenuItem.IsEnabled)
+				{
+					saveTemplatesMenuItem.IsEnabled = true;
+				*/
+				ResetTemplatesCommand ??= ReactiveCommand.Create(ResetTemplates);
+
+				if (TemplatePupilFinder.NumAntiTemplates > 0 && RemoveCurrentAntiTemplateCommand == null)
+					RemoveCurrentAntiTemplateCommand = ReactiveCommand.Create(RemoveCurrentAntiTemplate);
+			}
 		}
 
 		public void RemoveCurrentAntiTemplate()
 		{
-
+			if (TemplatePupilFinder != null)
+			{
+				TemplatePupilFinder.RemoveAntiTemplate(CurrentAntiTemplateIndex);
+				SetTemplatePreviewIndex(CurrentAntiTemplateIndex - 1);
+				if (TemplatePupilFinder.NumAntiTemplates < 1)
+					RemoveCurrentAntiTemplateCommand = null;
+			}
 		}
 
 		public void ChangeAntiTemplatePreviewIndex(int delta)
 		{
+			SetAntiTemplatePreviewIndex(CurrentAntiTemplateIndex + delta);
+		}
 
+
+		private void SetTemplatePreviewIndex(int index)
+		{
+			if (TemplatePupilFinder != null)
+			{
+				if (index < 0) index = 0;
+				else if (index >= TemplatePupilFinder.NumTemplates) index = TemplatePupilFinder.NumTemplates - 1;
+
+				CurrentTemplateIndex = index;
+				TemplatePreviewImage = TemplatePupilFinder.GetTemplateImage(index);
+			}
+		}
+
+		private void SetAntiTemplatePreviewIndex(int index)
+		{
+			if (TemplatePupilFinder != null)
+			{
+				if (index < 0) index = 0;
+				else if (index >= TemplatePupilFinder.NumAntiTemplates) index = TemplatePupilFinder.NumAntiTemplates - 1;
+
+				CurrentAntiTemplateIndex = index;
+				AntiTemplatePreviewImage = TemplatePupilFinder.GetAntiTemplateImage(index);
+			}
 		}
 	}
 }
--- SharpEyes/Views/TemplatePupilFinderConfigUserControl.axaml.cs
+++ SharpEyes/Views/TemplatePupilFinderConfigUserControl.axaml.cs
@@ -12,158 +12,22 @@ namespace SharpEyes.Views
 		{
 			get => (TemplatePupilFinderConfigUserControlViewModel)this.DataContext;
 		}
-
-		private PupilFindingUserControlViewModel PupilViewModel => viewModel?.Parent;
-		public TemplatePupilFinder? TemplatePupilFinder 
-		{
-			get
-			{
-				if (PupilFinder is TemplatePupilFinder templateFinder)
-					return templateFinder;
-				return null;
-			}
-		}
-
-		public PupilFinder? PupilFinder { get; set; } = null;
+		
 
 		public TemplatePupilFinderConfigUserControl()
 		{
 			InitializeComponent();
 		}
 
-		private void SetTemplatePreviewIndex(int index)
-		{
-			if (TemplatePupilFinder != null)
-			{
-				if (index < 0) index = 0;
-				else if (index >= TemplatePupilFinder.NumTemplates) index = TemplatePupilFinder.NumTemplates - 1;
-
-				viewModel.CurrentTemplateIndex = index;
-				viewModel.TemplatePreviewImage = TemplatePupilFinder.GetTemplateImage(index);
-			}
-		}
-
-		private void SetAntiTemplatePreviewIndex(int index)
-		{
-			if (TemplatePupilFinder != null)
-			{
-				if (index < 0) index = 0;
-				else if (index >= TemplatePupilFinder.NumAntiTemplates) index = TemplatePupilFinder.NumAntiTemplates - 1;
-
-				viewModel.CurrentAntiTemplateIndex = index;
-				viewModel.AntiTemplatePreviewImage = TemplatePupilFinder.GetAntiTemplateImage(index);
-			}
-		}
-
-		private void PreviousTemplateButton_OnClick(object? sender, RoutedEventArgs e)
-		{
-			SetTemplatePreviewIndex(viewModel.CurrentTemplateIndex - 1);
-		}
-
-		private void NextTemplateButton_OnClick(object? sender, RoutedEventArgs e)
-		{
-			SetTemplatePreviewIndex(viewModel.CurrentTemplateIndex + 1);
-		}
-
-		private void AddNewTemplateButton_OnClick(object? sender, RoutedEventArgs e)
-		{
-			if (TemplatePupilFinder != null)
-			{
-				int top = (int)(PupilViewModel.PupilY - PupilViewModel.PupilRadius * 1.5);
-				int bottom = (int)(PupilViewModel.PupilY + PupilViewModel.PupilRadius * 1.5 + 2);
-				int left = (int)(PupilViewModel.PupilX - PupilViewModel.PupilRadius * 1.5);
-				int right = (int)(PupilViewModel.PupilX + PupilViewModel.PupilRadius * 1.5 + 2);
-				TemplatePupilFinder.AddImageSegmentAsTemplate(top, bottom, left, right, PupilViewModel.PupilRadius);
-				SetTemplatePreviewIndex(TemplatePupilFinder.NumTemplates);
-
-				/* TODO: enabling buttons
-				if (!saveTemplatesMenuItem.IsEnabled)
-				{
-					saveTemplatesMenuItem.IsEnabled = true;
-				*/
-				ResetTemplatesButton.IsEnabled = true;
-				
-
-				if (TemplatePupilFinder.NumTemplates > 1)
-					RemoveTemplateButton.IsEnabled = true;
-			}
-		}
-
-		private void RemoveTemplateButton_OnClick(object? sender, RoutedEventArgs e)
-		{
-			if (TemplatePupilFinder != null)
-			{
-				TemplatePupilFinder.RemoveTemplate(viewModel.CurrentTemplateIndex);
-				SetTemplatePreviewIndex(viewModel.CurrentTemplateIndex - 1);
-				if (TemplatePupilFinder.NumTemplates < 2)
-					RemoveTemplateButton.IsEnabled = false;
-			}
-		}
-
-		private void ResetTemplatesButton_OnClick(object? sender, RoutedEventArgs e)
-		{
-			if (TemplatePupilFinder != null)
-			{
-				TemplatePupilFinder.MakeTemplates();
-				SetTemplatePreviewIndex(0);
-				ResetTemplatesButton.IsEnabled = false;
-				RemoveTemplateButton.IsEnabled = false;
-			}
-		}
-
-		private void PreviousAntiTemplateButton_OnClick(object? sender, RoutedEventArgs e)
-		{
-			SetAntiTemplatePreviewIndex(viewModel.CurrentAntiTemplateIndex - 1);
-		}
-
-		private void NextAntiTemplateButton_OnClick(object? sender, RoutedEventArgs e)
-		{
-			SetAntiTemplatePreviewIndex(viewModel.CurrentAntiTemplateIndex + 1);
-		}
-
-		private void AddNewAntiTemplateButton_OnClick(object? sender, RoutedEventArgs e)
-		{
-			if (TemplatePupilFinder != null)
-			{
-				int top = (int)(PupilViewModel.PupilY - PupilViewModel.PupilRadius * 1.5);
-				int bottom = (int)(PupilViewModel.PupilY + PupilViewModel.PupilRadius * 1.5 + 2);
-				int left = (int)(PupilViewModel.PupilX - PupilViewModel.PupilRadius * 1.5);
-				int right = (int)(PupilViewModel.PupilX + PupilViewModel.PupilRadius * 1.5 + 2);
-				TemplatePupilFinder.AddImageSegmentAsAntiTemplate(top, bottom, left, right);
-				SetAntiTemplatePreviewIndex(TemplatePupilFinder.NumAntiTemplates);
-
-				/* TODO: enabling buttons
-				if (!saveTemplatesMenuItem.IsEnabled)
-				{
-					saveTemplatesMenuItem.IsEnabled = true;
-				*/
-				ResetTemplatesButton.IsEnabled = true;
-
-
-				if (TemplatePupilFinder.NumAntiTemplates > 0)
-					RemoveAntiTemplateButton.IsEnabled = true;
-			}
-		}
-
-		private void RemoveAntiTemplateButton_OnClick(object? sender, RoutedEventArgs e)
-		{
-			if (TemplatePupilFinder != null)
-			{
-				TemplatePupilFinder.RemoveAntiTemplate(viewModel.CurrentAntiTemplateIndex);
-				SetTemplatePreviewIndex(viewModel.CurrentAntiTemplateIndex - 1);
-				if (TemplatePupilFinder.NumAntiTemplates < 1)
-					RemoveAntiTemplateButton.IsEnabled = false;
-			}
-		}
 
 		private void TemplatePreviewImage_OnPointerWheelChanged(object? sender, PointerWheelEventArgs e)
 		{
-			SetTemplatePreviewIndex(viewModel.CurrentTemplateIndex + (int)e.Delta.Y);
+			viewModel.ChangeTemplatePreviewIndex((int)e.Delta.Y);
 		}
 
 		private void AntiTemplatePreviewImage_OnPointerWheelChanged(object? sender, PointerWheelEventArgs e)
 		{
-			SetAntiTemplatePreviewIndex(viewModel.CurrentAntiTemplateIndex + (int)e.Delta.Y);
+			viewModel.ChangeAntiTemplatePreviewIndex((int)e.Delta.Y);
 		}
 	}
 }
